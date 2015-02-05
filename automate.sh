537b1563a3fb2e6881f09c3c3de97ced8ba988ea
welch <welch.canavan@gmail.com>
2015-02-05 17:43:44 -0500

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm install --global yeoman bower grunt-cli pure-prompt
+npm install --global yo bower grunt-cli http-server
 curl -L http://install.ohmyz.sh | sh
 ln -s /usr/local/lib/node_modules/pure-prompt/pure.zsh ~/.oh-my-zsh/custom/pure.zsh-theme
 ln -s code/dotfiles/.vimrc ~/.vimrc

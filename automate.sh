a7446903a12174d28e15f4bd9834b07bda7e7acb
Welch Canavan <welch.canavan@gmail.com>
2018-07-11 12:00:41 -0400

    remove outdated automation script

--- automate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm install --global yo bower grunt-cli http-server
-curl -L http://install.ohmyz.sh | sh
-ln -s /usr/local/lib/node_modules/pure-prompt/pure.zsh ~/.oh-my-zsh/custom/pure.zsh-theme
-ln -s code/dotfiles/.vimrc ~/.vimrc
-ln -s code/dotfiles/.zshrc ~/.zshrc

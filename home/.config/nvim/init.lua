30fbb7775fa8d59e15be47bcb61a9b1ba82d80bc
Welch Canavan <2304131+xiwcx@users.noreply.github.com>
2025-09-26 19:22:22 -0700

    consolidate theme

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -750,13 +750,13 @@ require('lazy').setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            vim.lsp.config[server_name] = server
           end,
         },
       }
 
       -- Haskell Language Server (installed via Homebrew, not Mason)
-      require('lspconfig').hls.setup {
+      vim.lsp.config.hls = {
         cmd = { 'haskell-language-server-wrapper', '--lsp' },
         filetypes = { 'haskell', 'lhaskell', 'cabal' },
         capabilities = capabilities,
@@ -925,25 +925,39 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+  { -- Gruvbox colorscheme to match Starship theme
+    -- This matches the gruvbox_dark palette used in your Starship configuration
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      require('gruvbox').setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false, -- Disable italics in comments to match your preference
+          operators = false,
+          folds = false,
         },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Load the colorscheme here - using the dark variant to match Starship
+      vim.cmd.colorscheme 'gruvbox'
+      -- Ensure we're using the dark background
+      vim.o.background = 'dark'
     end,
   },
 
